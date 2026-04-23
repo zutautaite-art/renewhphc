c0df01e1f554958a215df51342c52a99d85e4b05
zutautaite-art <zutautaite@gmail.com>
2026-04-23 07:47:41 +0100

    Revert "Add Vercel Blob shared workbook (cross-device) with /api routes"
    This reverts commit 64176d4c70c839f5a4bcb2d1fa7069e714d87f2e.

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'api']),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
